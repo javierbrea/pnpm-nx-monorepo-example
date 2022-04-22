70de06c56124199b231c1925a20b41e08771b260
javierbrea <javier.brea@gmail.com>
2022-04-22 13:46:32 +0200

    chore: Fix workflow

--- scripts/print-affected-array.js
+++ scripts/print-affected-array.js
@@ -1,3 +1,3 @@
 const processArguments = process.argv.slice(2);
 
-console.log({ task: processArguments[1], branch: processArguments[1] });
+console.log({ task: processArguments[0], branch: processArguments[1] });
